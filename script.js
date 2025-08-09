4c8cf98eb6ea0c4f923a9f042c955a11c1f13218
roshni <roshnichauhan128@gmail.com>
2025-08-09 18:40:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("alertBtn").addEventListener("click", function() {
+    alert("Hello! You clicked the button. 🚀");
+});
+
+document.getElementsById("alertBtn").addEventListener("click", function() {
+    alert("THANK YOU for visting .");
+});throw new Error("This is a test error message.");
