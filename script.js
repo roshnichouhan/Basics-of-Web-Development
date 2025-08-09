f2334e17445e22db727bc2b7c88a2604ace29fe6
roshni <roshnichauhan128@gmail.com>
2025-08-09 18:49:46 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-document.getElementById("alertBtn").addEventListener("click", function() {
-    alert("Hello! You clicked the button. 🚀");
-});
-
-document.getElementsById("alertBtn").addEventListener("click", function() {
-    alert("THANK YOU for visting .");
-});throw new Error("This is a test error message.");
